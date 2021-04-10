5bf13bba076d9e9046759cccb903cf6c592ca114
harisnasrull <harisnasrullah212@gmail.com>
2021-04-10 16:46:22 +0700

    add mock testing

--- UnitTesting/app/src/test/java/com/nasuru/unittesting/viewmodel/MainViewModelTest.kt
+++ UnitTesting/app/src/test/java/com/nasuru/unittesting/viewmodel/MainViewModelTest.kt
@@ -5,7 +5,7 @@ import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 
 class MainViewModelTest {
 
@@ -55,5 +55,27 @@ class MainViewModelTest {
 
     @Test
     fun save() {
+    }
+
+    @Test
+    fun testMockVolume() {
+        `when`(mainViewModel.getVolume()).thenReturn(dummyVolume)
+        val volume = mainViewModel.getVolume()
+        verify(cuboidModel).getVolume()
+        assertEquals(dummyVolume, volume, 0.0001)
+    }
+    @Test
+    fun testMockCircumference() {
+        `when`(mainViewModel.getCircumference()).thenReturn(dummyCircumference)
+        val circumference = mainViewModel.getCircumference()
+        verify(cuboidModel).getCircumference()
+        assertEquals(dummyCircumference, circumference, 0.0001)
+    }
+    @Test
+    fun testMockSurfaceArea() {
+        `when`(mainViewModel.getSurfaceArea()).thenReturn(dummySurfaceArea)
+        val surfaceArea = mainViewModel.getSurfaceArea()
+        verify(cuboidModel).getSurfaceArea()
+        assertEquals(dummySurfaceArea, surfaceArea, 0.0001)
     }
 }
